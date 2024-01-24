e455db173c1f9b08aacbc7672c1458738ed21a60
ArtinaAzemi1 <artina.azemi@gmail.com>
2024-01-24 21:43:25 +0100

    E kena nda validimin e login me ni file te veqante edhe disa ndryshime te vogla rreth stilizimit

---
+++ login.js
@@ -0,0 +1,32 @@
+var usernameRegex = /^[A-Za-z]{9,14}$/;
+var passwordRegex = /^[A-Z][a-z]{2,8}[0-9]{0,3}$/;
+
+var loginButton = document.getElementById("login");
+var usernameMessage = document.getElementById("usernameMsg");
+var passwordMessage = document.getElementById("passwordMsg");
+
+loginButton.addEventListener("click", function(event) {
+    var username = document.getElementById("Username").value;
+    var password = document.getElementById("Password").value;
+
+    usernameMessage.innerText = "";
+    passwordMessage.innerText = "";
+
+    if(username == "") {
+        usernameMessage.innerText = "Username is required !";
+        event.preventDefault();
+    } else if (!usernameRegex.test(username)) {
+        usernameMessage.innerText = 'Invalid username.';
+        event.preventDefault();
+    }
+
+    if (password == "") {
+        passwordMessage.innerText = "Password is required !";
+        event.preventDefault();
+    } else if (!passwordRegex.test(password)) {
+        passwordMessage.innerText = 'Invalid password .';
+        event.preventDefault();
+    } else {
+        alert('Username and Password are correct!');
+    }
+});
