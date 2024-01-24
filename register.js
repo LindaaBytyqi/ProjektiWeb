e2544a22631bc2c5a44f42490c0df7fa20414669
ArtinaAzemi1 <artina.azemi@gmail.com>
2024-01-24 21:30:38 +0100

    Modifikime te register forma

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 let nameRegex = /^[A-Z][a-z]{2,8}$/;
 let emailRegex = /^[a-z]+[._-]?\w+@+[a-z]+\.[a-z]{2,3}$/;
-let passwordRegex = /^[A-Z][a-z]{3,6}$/;
+let passwordRegex = /^[A-Z][a-z]{3,6}$|^[A-Z][a-z]*([a-z]\d*){3,6}\d$/;
 let numberRegex = /^[0-9+]{2,8}$/;
 function validimi() {
     
@@ -19,35 +19,45 @@ function validimi() {
     passError.textContent = '';
     nameError.textContent = '';
 
+    let isValid = true;
+
     if(nameField.value == "") {
-        nameError.textContent="Filling this field is required."
-        event.preventDefault();
+        nameError.textContent="Filling this field is required.";
+        isValid = false;
     }else if(!nameRegex.test(nameField.value)){
-        nameError.textContent = 'The full name you have entered is incorrect. Try again.';
-        event.preventDefault();
+        nameError.textContent = 'Incorrect full name! Try again.';
+        isValid = false;
     }
 
     if(emailField.value == "") {
-        emailError.textContent="Filling this field is required."
-        event.preventDefault();
+        emailError.textContent="Filling this field is required.";
+        isValid = false;
     } else if(!emailRegex.test(emailField.value)){
-        emailError.textContent = 'The email you have entered is incorrect. Try again.';
-        event.preventDefault();
+        emailError.textContent = 'Incorrect email! Try again.';
+        isValid = false;
     }
 
     if(passwordField.value == "") {
         passwordError.textContent="Filling this field is required."
-        event.preventDefault();
+        isValid = false;
     } else if(!passwordRegex.test(passwordField.value)){
-        passwordError.textContent = 'The password you have entered is incorrect. Try again.';
-        event.preventDefault();
+        passwordError.textContent = 'Incorrect password! Try again.';
+        isValid = false;
     }
 
     if(numberField.value == "") {
         numberError.textContent="Filling this field is required."
-        event.preventDefault();
+        isValid = false;
     } else if(!numberRegex.test(numberField.value)){
-        numberError.textContent = 'The phone number you have entered is incorrect. Try again.';
+        numberError.textContent = 'Incorrect phone number! Try again.';
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert('The form has been successfully filled!');
+    }
+
+    if (!isValid) {
         event.preventDefault();
     }
 }
