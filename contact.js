da159ec14f862c7bcdfa1110bb197a485524d271
Linda Bytyqi <lb67786@ubt-uni.net>
2024-01-25 17:00:20 +0100

    Slideri

--- contact.js
+++ contact.js
@@ -1,13 +1,13 @@
-let nameRegex = /^[a-zA-Z]{9,14}$/;
-let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-let phoneRegex = /^\+[0-9]{6,14}$/;
+let nameRegex = /^[a-zA-Z]{2,12}$/;
+let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-z]{2,4}$/;
+let phoneRegex = /^[0-9+]{6,14}$/;
 let messageRegex = /^[a-zA-Z0-9,'.;"!?]{10,50}$/;
 
 function validimi() {
-    let nameField = document.getElementById("Name");
-    let emailField = document.getElementById("Email");
-    let phoneField = document.getElementById("Phone");
-    let messageField = document.getElementById("Message");
+    let nameField = document.getElementById("name");
+    let emailField = document.getElementById("email");
+    let phoneField = document.getElementById("phone");
+    let messageField = document.getElementById("message");
 
     let nameError = document.getElementById("nameError");
     let emailError = document.getElementById("emailError");
@@ -19,12 +19,13 @@ function validimi() {
     phoneError.textContent = '';
     messageError.textContent = '';
 
+
     if (nameField.value == "") {
         nameError.textContent = "Filling this field is required";
         event.preventDefault();
     } else if (!nameRegex.test(nameField.value)) {
         nameError.textContent = "Invalid name";
-        event.preventDefault();
+        isValid.preventDefault();
     }
 
     if (emailField.value == "") {
