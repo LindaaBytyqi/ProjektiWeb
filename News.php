3b4362c627cdd3f92ff47cddb691ddfdd588c19b
ArtinaAzemi1 <artina.azemi@gmail.com>
2024-02-03 13:51:35 +0100

    Modifikime te faqja e News

--- News.php
+++ News.php
@@ -18,11 +18,11 @@
             <h2 class="logo">FIDANISHTJA BYTYQI</h2>
             <input type='checkbox' id='check' />
             <span class="menu">
-                <li><a href="Home.html">Home</a></li>
-                <li><a href="aboutus.html">About</a></li>
-                <li><a href="product.html">Products</a></li>
-                <li><a href="News.html">News</a></li>
-                <li><a href="contactus.html">Contact</a></li>
+                <li><a href="Home.php">Home</a></li>
+                <li><a href="aboutus.php">About</a></li>
+                <li><a href="product.php">Products</a></li>
+                <li><a href="News.php">News</a></li>
+                <li><a href="contactus.php">Contact</a></li>
                 <a href="login.php"><button class="Loginbtn">Log in</button></a>
                 <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
             </span>
@@ -41,7 +41,7 @@
                     <img src="bella.jpg" alt="Blog">
                 </div>
                 <div class="kontenti">
-                    <span>22 January 2023</span>
+                    <span>22 January 2024</span>
                     <a href="#" class="main-new">Timeless Elegance: Charm of Flower Arrangements</a>
                     <p>Step into an era of timeless elegance with our classic flower arrangement. This bouquet transcends trends, embracing a refined charm that never goes out of style. Uncover the beauty of traditional floral artistry, meticulously crafted to add a touch of sophistication to your space.</p>
                     <a href="#" class="Read">Read More</a>
@@ -53,7 +53,7 @@
                     <img src="c3.jpg" alt="Blog">
                 </div>
                 <div class="kontenti">
-                    <span>21 January 2023</span>
+                    <span>21 January 2024</span>
                     <a href="#" class="main-new">Warmth Map: The Flower Bouquet to Soften Hearts</a>
                     <p>A unique beauty has been added to our collection of flower bouquets. The latest bouquet is a warmth map with carefully selected flowers to bring joy and tranquility to any space. Browse to find the bouquet that will soften your heart and bring a touch of magic to your life!</p>
                     <a href="#" class="Read">Read More</a>
@@ -65,7 +65,7 @@
                     <img src="wh.jpg" alt="Blog">
                 </div>
                 <div class="kontenti">
-                    <span>18 January 2023</span>
+                    <span>18 January 2024</span>
                     <a href="#" class="main-new">Spring's Elegance: Unveiling the Latest Flower Bouquet</a>
                     <p>This spring, a new beauty has blossomed in the world of flowers. The latest flower bouquet has brought forth a unique harmony of colors, shapes, and scents, making every moment truly radiant. Explore to discover the newest bouquet that is becoming the highlight of the season!</p>
                     <a href="#" class="Read">Read More</a>
@@ -76,7 +76,7 @@
                     <img src="gardden.jpg" alt="Blog">
                 </div>
                 <div class="kontenti">
-                    <span>16 January 2023</span>
+                    <span>16 January 2024</span>
                     <a href="#" class="main-new">Garden Symphony: Harmonious Bouquet of Blooms</a>
                     <p>Immerse yourself in the garden symphony with our harmonious bouquet of blooms. Each flower contributes to the orchestration of colors and scents, creating a delightful melody for the senses. Let this enchanting arrangement transform your space into a botanical concert, celebrating the beauty of nature's composition.</p>
                     <a href="#" class="Read">Read More</a>
@@ -88,7 +88,7 @@
                     <img src="02.webp" alt="Blog">
                 </div>
                 <div class="kontenti">
-                    <span>15 January 2023</span>
+                    <span>15 January 2024</span>
                     <a href="#" class="main-new">Whispers of Nature: Embracing the Serenity in Every Petal</a>
                     <p>Embark on a journey through the whispers of nature with our latest flower bouquet. Each petal tells a story of tranquility and natural elegance. Discover the bouquet that resonates with the calming essence of the great outdoors, bringing a touch of serenity to your surroundings.</p>
                     <a href="#" class="Read">Read More</a>
@@ -100,7 +100,7 @@
                     <img src="20fa.jpg" alt="Blog">
                 </div>
                 <div class="kontenti">
-                    <span>11 January 2023</span>
+                    <span>11 January 2024</span>
                     <a href="#" class="main-new">Symphony of Fragrance: A Bouquet That Sings the Language of Flowers</a>
                     <p>Indulge your senses in the symphony of fragrance crafted in our newest flower bouquet. Each bloom harmonizes to create a melody that speaks the language of flowers. Immerse yourself in the aromatic masterpiece, where every petal contributes to a sensorial experience like no other.</p>
                     <a href="#" class="Read">Read More</a>
